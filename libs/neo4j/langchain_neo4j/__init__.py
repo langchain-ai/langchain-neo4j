908f29a82a81e25381bdf8865ddce8bc3bbb9bfe
Tomaz Bratanic <bratanic.tomaz@gmail.com>
2026-01-16 00:11:54 +1100

    Add neo4j checkpoint saver (#90)
    * Add neo4j checkpoint saver
    
    * format
    
    * linting
    
    * linting
    
    * linting
    
    * readme
    
    * lint
    
    * test imports fix

--- libs/neo4j/langchain_neo4j/__init__.py
+++ libs/neo4j/langchain_neo4j/__init__.py
@@ -2,6 +2,8 @@ from importlib import metadata
 
 from langchain_neo4j.chains.graph_qa.cypher import GraphCypherQAChain
 from langchain_neo4j.chat_message_histories.neo4j import Neo4jChatMessageHistory
+from langchain_neo4j.checkpoint import Neo4jSaver
+from langchain_neo4j.checkpoint.aio import AsyncNeo4jSaver
 from langchain_neo4j.graphs.neo4j_graph import Neo4jGraph
 from langchain_neo4j.vectorstores.neo4j_vector import Neo4jVector
 
@@ -13,9 +15,11 @@ except metadata.PackageNotFoundError:
 del metadata  # optional, avoids polluting the results of dir(__package__)
 
 __all__ = [
+    "AsyncNeo4jSaver",
     "GraphCypherQAChain",
     "Neo4jChatMessageHistory",
     "Neo4jGraph",
+    "Neo4jSaver",
     "Neo4jVector",
     "__version__",
 ]
